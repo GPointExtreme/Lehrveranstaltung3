2dda51b4546f3581d98b524c1c8910321cee37f8
GPointExtreme <dominik.geiger@hotmail.com>
2015-11-13 14:09:11 +0100

    for und while Schleife

--- src/LoopDemo.java
+++ src/LoopDemo.java
@@ -2,7 +2,17 @@
 public class LoopDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Unterschied zwischen for und while Schleife!
+		
+		for(int i=1; i <= 10; i++) {
+			System.out.println("Durchlauf: " + i);
+		}
+		
+		int j=1;
+		while(j <= 10) {
+			System.out.println("Durchlauf " + j);
+			j++;
+		}
 
 	}
 
