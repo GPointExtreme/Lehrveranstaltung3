d2f9f744019458ad5aaebf0bcd9ee907a75a6909
GPointExtreme <dominik.geiger@hotmail.com>
2015-11-18 21:19:35 +0100

    Einheitsmatrix verbessert in 2 Methoden!

--- src/Einheitsmatrix.java
+++ src/Einheitsmatrix.java
@@ -4,22 +4,26 @@ public class Einheitsmatrix {
 	public static void main(String[] args) {
 		// Quadratische Matrix diagonale generieren
 		
-		generatematrix(5);
+		printMatrix(generatematrix(5));
 
 	}
-				
+	
+	//Matrix wird hier gebaut mit lauter 0er
+	public static void printMatrix(double[][] matrix) {
+		for(int i=0; i < matrix.length; i++) {
+			for(int n=0; n < matrix.length; n++) {
+				System.out.print(matrix[i][n]+"\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	//wenn Spalte und Zeile gleich sind soll ein 1er ausgegeben werden
 	public static double [][] generatematrix(int size) {
 		double [][] matrix = new double [size][size];
 		
 		for (int i=0; i < size; i++) {
-			
-			for (int j=0; j < size; j++) {
-				if(i==j) {
-					matrix [i][j]=1.0;
-				}
-			System.out.print(matrix[i][j]+" ");
-			}
-			System.out.println();
+			matrix [i][i]=1.0;
 		}
 		return matrix;
 	}
